4796ca1c78f3ba53b050df4459ea11472b8b3994
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-25 15:19:04 +0600

    added different routes/pages with their respective index.js file

---
+++ pages/events/[...slug]/index.js
@@ -0,0 +1,7 @@
+function EventsWithSlugPage() {
+    return <div>
+        <h1>Events with SLUG Page</h1>
+    </div>
+}
+
+export default EventsWithSlugPage;
---
+++ pages/events/[some-id]/index.js
@@ -0,0 +1,7 @@
+function EventWithIdPage() {
+    return <div>
+        <h1>Events Page With ID</h1>
+    </div>
+}
+
+export default EventWithIdPage;
